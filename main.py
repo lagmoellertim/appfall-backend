5605f1031d5386edba5613f2384bf18b6840e6f8
Tim-Luca <mail@lagmoellertim.de>
2022-09-23 14:35:08 +0200

    Added basic view models

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import List, Union
 
 from fastapi import FastAPI
 
+from view_models import DisposalSiteModel, DisposalItemModel, EventModel, QueryResultModel
+
 app = FastAPI()
 
 
@@ -10,18 +12,18 @@ async def get_decision_tree():
     return {"message": "Hello World"}
 
 
-@app.get("/disposal_sites")
+@app.get("/disposal_sites", response_model=List[DisposalSiteModel])
 async def get_disposal_sites(long: Union[float, None] = None, lat: Union[float, None] = None,
                              radius: Union[int, None] = None, tags: Union[List[str], None] = None):
     return {}
 
 
-@app.get("/disposal_items/{item_id}")
+@app.get("/disposal_items/{item_id}", response_model=DisposalItemModel)
 async def get_disposal_item(item_id: str):
     return {}
 
 
-@app.get("/events")
+@app.get("/events", response_model=List[EventModel])
 async def get_events(
         long: Union[float, None] = None,
         lat: Union[float, None] = None,
@@ -31,11 +33,11 @@ async def get_events(
     return {}
 
 
-@app.get("/search/query")
+@app.get("/search/query", response_model=List[QueryResultModel])
 async def query_search(query: str):
     return {}
 
 
-@app.post("/search/image")
+@app.post("/search/image", response_model=List[QueryResultModel])
 async def image_search():
     return {}
--- view_models.py
+++ view_models.py
@@ -3,9 +3,33 @@ from typing import Union, List
 from pydantic import BaseModel
 
 
-class Item(BaseModel):
+class LocationModel(BaseModel):
+    lang: float
+    lat: float
+
+
+class DisposalSiteModel(BaseModel):
+    id: str
     name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: List[str] = []
+    info: str
+    location: LocationModel
+    tags: List[str]
+
+
+class DisposalItemModel(BaseModel):
+    id: str
+    name: str
+    tags: List[str]
+
+
+class EventModel(BaseModel):
+    id: str
+    name: str
+    timestamp: int
+    location: Union[LocationModel, str]
+
+
+class QueryResultModel(BaseModel):
+    id: str
+    name: str
+    confidence: float
