7f39e591f28fc0bbc0ec12ce897c99a99c7737a8
Tim-Luca <mail@lagmoellertim.de>
2022-09-24 12:54:19 +0200

    Fixed AI Loading

--- main.py
+++ main.py
@@ -59,9 +59,10 @@ def startup_db_client():
     app.mongodb_client = MongoClient(connection_string)
     app.db = app.mongodb_client["appfall"]
     app.db["disposal_sites"].create_index([("location", pymongo.GEOSPHERE)])
-    #app.model = pt_multilingual_clip.MultilingualCLIP.from_pretrained(model_name, cache_dir="clip")
-    #app.clip_model, app.clip_preprocess = clip.load(clip_model_name, device="cpu", download_root="clip")
-    #app.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    if os.environ.get("LOAD_AI") == "true":
+        app.model = pt_multilingual_clip.MultilingualCLIP.from_pretrained(model_name, cache_dir="clip")
+        app.clip_model, app.clip_preprocess = clip.load(clip_model_name, device="cpu", download_root="clip")
+        app.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
 
     print("Connected to the MongoDB database!")
 
